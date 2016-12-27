9a9be298cf513b88ee153932deef1a0356866220
ShaoranD3 <leonesqueda@uip.edu.pa>
2016-12-27 01:26:57 -0500

    Archivos editados para el funcionamiento de la app

---
+++ pages/leo/leo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-leo',
+  templateUrl: 'leo.html'
+})
+export class LeoPage {
+  constructor() {
+
+  }
+}
